8bbf99752e16353c2bddeffb9132dced9918da3c
Hediye Ersan <hediyesnl9@gmail.com>
2025-05-30 16:56:28 +0300

    created Aplication and AplicationTest

---
+++ src/main/java/com/example/Kisisel_Asistan/KisiselAsistanApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kisisel_Asistan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisiselAsistanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KisiselAsistanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Kisisel_Asistan/KisiselAsistanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kisisel_Asistan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KisiselAsistanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
