200122ead4c4b25a3602675b6d3af19c5f1f8ed0
Hediye Ersan <hediyesnl9@gmail.com>
2025-07-02 16:30:50 +0300

    Created new List

--- src/main/java/com/example/Kisisel_Asistan/Repository/HabitRepository.java
+++ src/main/java/com/example/Kisisel_Asistan/Repository/HabitRepository.java
@@ -1,10 +1,12 @@
 package com.example.Kisisel_Asistan.Repository;
 
 import com.example.Kisisel_Asistan.Entity.Habit;
+import com.example.Kisisel_Asistan.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface HabitRepository extends JpaRepository<Habit, Long> {
     List<Habit> findByUserId(Long userId);
+    List<Habit> findByUser(User user);
 }
